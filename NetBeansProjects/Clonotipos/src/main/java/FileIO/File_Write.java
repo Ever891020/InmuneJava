c3c57a4ae9480de37a272f8b8bea0d47efd57133
Ever891020 <ever.gmillan@gmail.com>
2017-10-19 09:40:17 -0500

    Merge origin/master
    Conflicts:
    	src/main/java/Data/FileRead.java

---
+++ NetBeansProjects/Clonotipos/src/main/java/FileIO/File_Write.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FileIO;
+
+/**
+ *
+ * @author ever
+ */
+public class File_Write {
+    
+}
